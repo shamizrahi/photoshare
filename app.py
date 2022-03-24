8855da4cbcf0bf9fbbe2ac75871a8e34505adcc6
shamizrahi <66646726+shamizrahi@users.noreply.github.com>
2022-03-24 14:10:16 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
-######################################
-# author ben lawson <balawson@bu.edu>
-# Edited by: Craig Einstein <einstein@bu.edu>
-######################################
-# Some code adapted from
-# CodeHandBook at http://codehandbook.org/python-web-application-development-using-flask-and-mysql/
-# and MaxCountryMan at https://github.com/maxcountryman/flask-login/
-# and Flask Offical Tutorial at  http://flask.pocoo.org/docs/0.10/patterns/fileuploads/
-# see links for further understanding
-###################################################
-
 import flask
-from flask import Flask, Response, request, render_template, redirect, url_for
+from flask import Flask, Response, request, render_template, redirect, url_for, flash
 from flaskext.mysql import MySQL
 import flask_login
+import time
+from werkzeug.utils import secure_filename
 
 #for image uploading
 import os, base64
 
 mysql = MySQL()
 app = Flask(__name__)
-app.secret_key = 'super secret string'  # Change this!
+app.secret_key = 'cs460'  # Change this!
 
 #These will need to be changed according to your creditionals
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -123,15 +114,18 @@ def register_user():
 	try:
 		email=request.form.get('email')
 		password=request.form.get('password')
+		first_name=request.form.get('first_name')
+		last_name=request.form.get('last_name')
+		dob=request.form.get('dob')
+		hometown=request.form.get('hometown')
+		gender=request.form.get('gender')
 	except:
-		print("couldn't find all tokens") #this prints to shell, end users will not see this (all print statements go to shell)
 		return flask.redirect(flask.url_for('register'))
 	cursor = conn.cursor()
 	test =  isEmailUnique(email)
 	if test:
-		print(cursor.execute("INSERT INTO Users (email, password) VALUES ('{0}', '{1}')".format(email, password)))
+		print(cursor.execute("INSERT INTO Users (email, password, first_name, last_name, dob, hometown, gender) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}')".format(email, password, first_name, last_name, dob, hometown, gender)))
 		conn.commit()
-		#log user in
 		user = User()
 		user.id = email
 		flask_login.login_user(user)
@@ -143,22 +137,19 @@ def register_user():
 def getUsersPhotos(uid):
 	cursor = conn.cursor()
 	cursor.execute("SELECT imgdata, picture_id, caption FROM Pictures WHERE user_id = '{0}'".format(uid))
-	return cursor.fetchall() #NOTE return a list of tuples, [(imgdata, pid, caption), ...]
+	return cursor.fetchall() 
 
 def getUserIdFromEmail(email):
 	cursor = conn.cursor()
-	cursor.execute("SELECT user_id  FROM Users WHERE email = '{0}'".format(email))
+	cursor.execute("SELECT user_id FROM Users WHERE email = '{0}'".format(email))
 	return cursor.fetchone()[0]
 
 def isEmailUnique(email):
-	#use this to check if a email has already been registered
 	cursor = conn.cursor()
 	if cursor.execute("SELECT email  FROM Users WHERE email = '{0}'".format(email)):
-		#this means there are greater than zero entries with that email
 		return False
 	else:
 		return True
-#end login code
 
 @app.route('/profile')
 @flask_login.login_required
@@ -178,22 +169,320 @@ def upload_file():
 		uid = getUserIdFromEmail(flask_login.current_user.id)
 		imgfile = request.files['photo']
 		caption = request.form.get('caption')
+		tags = request.form.get('tag')
+		album_name = request.form.get('album_name')
 		photo_data =imgfile.read()
 		cursor = conn.cursor()
-		cursor.execute('''INSERT INTO Pictures (imgdata, user_id, caption) VALUES (%s, %s, %s )''', (photo_data, uid, caption))
+		
+		cursor.execute("SELECT album_name FROM Albums WHERE user_id = '{0}'".format(uid))
+		albums = cursor.fetchall()
+		album_exists = False
+		for i in range(len(albums)):
+			if album_name == str(albums[i][0]):
+				album_exists = True
+		if album_exists == False:
+			return render_template('hello.html', message='Album does not exist yet.')
+		cursor.execute("SELECT album_id FROM Albums WHERE user_id = '{0}' AND album_name = '{1}'".format(uid, album_name))
+		album_id = cursor.fetchone()[0]
+		cursor.execute('''INSERT INTO Pictures (imgdata, user_id, caption, album_id) VALUES (%s, %s, %s, %s)''', (photo_data, uid, caption, album_id))
 		conn.commit()
+		
+		pic_id = cursor.lastrowid
+
+		tags = tags.split()
+		num_tag = len(tags) - 1 
+		cursor = conn.cursor()
+
+		while num_tag >= 0:
+			cursor.execute("INSERT INTO Tags(tag) VALUES ('{0}')".format(tags[num_tag]))
+			conn.commit()
+			tag_id = cursor.lastrowid
+			cursor.execute("INSERT INTO Tagged_With(picture_id, tag_id) VALUES ('{0}', '{1}')".format(pic_id, tag_id))
+			conn.commit()
+			num_tag -= 1
 		return render_template('hello.html', name=flask_login.current_user.id, message='Photo uploaded!', photos=getUsersPhotos(uid), base64=base64)
-	#The method is GET so we return a  HTML form to upload the a photo.
 	else:
 		return render_template('upload.html')
-#end photo uploading code
 
+@app.route('/create_album', methods=['GET', 'POST'])
+def createAlbum():
+	if request.method == 'POST':
+		uid = getUserIdFromEmail(flask_login.current_user.id)
+		name = request.form.get('album_name')
+		date = time.strftime('%Y-%m-%d')
+		cursor = conn.cursor()
+		cursor.execute("INSERT INTO Albums(album_name, user_id, album_date) VALUES ('{0}','{1}','{2}')".format(name, uid, date))
+		conn.commit()
+		return render_template('hello.html', message='Your album has been created!')
+	return render_template('create_album.html')
+
+def mostRecentUserPhotos(uid):
+	cursor = conn.cursor()
+	cursor.execute("SELECT P.imgdata, P.picture_id, P.caption, U.first_name, U.last_name, P.user_id FROM Pictures P, Users U WHERE P.user_id = U.user_id AND P.user_id = '{0}' ORDER BY P.picture_id DESC".format(uid))
+	return cursor.fetchall()
+
+@app.route('/display_pictures', methods=['GET', 'POST'])
+def displayUserPhotos():
+	uid = getUserIdFromEmail(flask_login.current_user.id)
+	if request.method == 'POST':
+		photo_id = request.form.get('picture_id')
+		deletePicture(photo_id)
+		return render_template('display_pictures.html', name=flask_login.current_user.id, user= uid,message='Your photos', photos=mostRecentUserPhotos(uid), base64=base64)
+	return render_template('display_pictures.html', name=flask_login.current_user.id, user= uid,message='Your photos', photos=mostRecentUserPhotos(uid), base64=base64)
+
+def getUsersAlbums(uid):
+	cursor = conn.cursor()
+	cursor.execute("SELECT album_id, album_name, album_date FROM Albums WHERE user_id = '{0}'".format(uid))
+	return cursor.fetchall()
+
+def getAlbumsPhotos(album_id):
+	cursor = conn.cursor()
+	cursor.execute("SELECT picture_id, imgdata, caption FROM Pictures WHERE album_id = '{0}'".format(album_id))
+	return cursor.fetchall() 
+
+@app.route('/viewalbums', methods = ['GET']) 
+@flask_login.login_required
+def view_albums():
+	uid =  getUserIdFromEmail(flask_login.current_user.id)
+	myAlbums = getUsersAlbums(uid) 
+	return render_template("albums.html", message="Here are all of your albums", name=flask_login.current_user.id, albums=myAlbums)
+
+@app.route('/viewalbums/<album_id>', methods = ['GET']) # album_id here is album[0] in albums.html
+@flask_login.login_required
+def view_photos_in_album(album_id):
+	myPhotos = getAlbumsPhotos(album_id)
+	return render_template("photos.html", message="Photos in this album", albumid=album_id, photos=myPhotos, base64=base64)
+
+@app.route('/deletealbum/<album_id>')
+@flask_login.login_required
+def delete_album(album_id):
+	uid =  getUserIdFromEmail(flask_login.current_user.id)
+	cursor = conn.cursor()
+	cursor.execute("DELETE FROM Albums WHERE album_id =('{0}')".format(album_id))
+	conn.commit()
+	myAlbums = getUsersAlbums(uid)
+	return render_template("albums.html", message="Album deleted", albums=myAlbums)
+
+@app.route('/deletephoto/<album_id>/<photo_id>')
+@flask_login.login_required
+def delete_photo(album_id, photo_id):
+	cursor = conn.cursor()
+	cursor.execute("DELETE FROM Tagged_With WHERE picture_id ='{0}'".format(photo_id))
+	cursor.execute("DELETE FROM Pictures WHERE picture_id ='{0}'".format(photo_id))
+	conn.commit()
+	myPhotos = getAlbumsPhotos(album_id)
+	return render_template("photos.html", message="Photo deleted", photos=myPhotos, albumid=album_id)
+
+# FRIENDS #
+def getEmailfromUid(uid):
+	cursor = conn.cursor()
+	if cursor.execute ("SELECT email FROM Users WHERE user_id = '{0}'".format(uid)):
+		return False
+	else:
+		return True		# query to display friends
+
+def getUserNameFromId(user_id):
+	cursor = conn.cursor()
+	cursor.execute("SELECT first_name, last_name FROM Users WHERE user_id = '{0}'".format(user_id))
+	return cursor.fetchone()[0] # query to search friends
+
+def getUsersFriends(uid):
+	cursor = conn.cursor()
+	cursor.execute("SELECT user_id FROM Friends WHERE friend_id = '{0}'".format(uid))
+	return cursor.fetchall() # query to add frineds
+
+def AddFriend(uid, friend_id):
+	cursor = conn.cursor()
+	cursor.execute("INSERT INTO Friends (user_id, friend_id) VALUES ('{0}', '{1}')".format(uid, friend_id))
+	conn.commit()	#add to friends table
+
+@app.route('/friends', methods = ['GET'])
+@flask_login.login_required
+def friends():
+	uid = getUserIdFromEmail(flask_login.current_user.id) #getting email
+	cursor = conn.cursor()
+	cursor.execute("SELECT friend_id FROM Friends WHERE user_id = '{0}'".format(uid)) #find friends relationship
+	friends = cursor.fetchall() 
+	f_name = []
+	for i in friends: 
+		f_name.append(getUserNameFromId(i[0])) #get names for each friend
+	return render_template("friends.html", name=flask_login.current_user.id, friends=f_name)
+
+@app.route('/search_add', methods = ['GET']) 
+@flask_login.login_required
+def find_friend():
+	return render_template('search_add.html', name=flask_login.current_user.id)
+
+@app.route('/search_add', methods = ['POST']) 
+@flask_login.login_required
+def search():
+	if request.method == 'POST':
+		email = request.form.get('?')
+		cursor = conn.cursor()
+		cursor.execute("SELECT user_id, first_name, last_name from Users WHERE email = '{0}'".format(email))
+		results = cursor.fetchall()
+		return render_template("search_add.html", name=flask_login.current_user.id, results = results)
+
+@app.route('/addfriends/<uid_friend>')
+@flask_login.login_required
+def add_friend(uid_friend):
+	uid =  getUserIdFromEmail(flask_login.current_user.id)
+	cursor = conn.cursor()
+	cursor.execute("INSERT INTO Friends (user_id, friend_id) VALUES ('{0}', '{1}')".format(uid, uid_friend))
+	conn.commit()
+	friends = getUsersFriends(uid)
+	fname = []
+	for i in friends:
+		fname.append(getUserNameFromId(i[0]))
+	return render_template("friends.html", message="New friend added", friends=fname)
+
+# TAGS #
+
+def getTagName(tag):
+	cursor = conn.cursor()
+	cursor.execute("""SELECT DISTINCT t.tag
+						FROM Pictures AS p, Tags AS t, Tagged_With AS w
+						WHERE t.tag_id = w.tag_id 
+						AND w.picture_id = p.picture_id""".format(tag))
+	result = cursor.fetchall()
+	return result
+
+def getTaggedPhotos(tag):
+	cursor = conn.cursor()
+	cursor.execute("""SELECT DISTINCT p.imgdata, p.picture_id, t.tag_id
+						FROM Pictures AS p, Tags AS t, Tagged_With AS w
+						WHERE t.tag_id = w.tag_id 
+						AND p.picture_id = w.picture_id""".format(tag))
+	result = cursor.fetchall()
+	return result
+    	
+def getUsersTaggedPhotos(tag):
+	cursor = conn.cursor()
+	cursor.execute("""SELECT DISTINCT p.imgdata, p.picture_id, t.tag_id
+					FROM Tags AS t, Tagged_With AS w, Pictures AS p  
+					WHERE t.tag_id = w.tag_id
+					and w.picture_id = p.picture_id""".format(tag))
+	return cursor.fetchall()
+
+@app.route('/tags_and_likes', methods=['GET', 'POST'])
+def taggedPhotos():
+	tag = request.form.get('tag')
+	myPhotos = getTaggedPhotos(tag)
+	tags = getTagName(tag)
+	return render_template('tags.html', photos=myPhotos, tags = tags, base64=base64)  			
+
+@app.route('/usertags', methods=['GET', 'POST'])
+@flask_login.login_required
+def usersTaggedPhotos():
+	tag = request.form.get('tag_id')
+	photos = getUsersTaggedPhotos(tag)
+	tags = getTagName(tag)
+	return render_template('usertags.html', photos=photos, tags = tags, base64=base64) 
+
+# COMMENTS #
+
+def getComments(photo_id):
+	cursor = conn.cursor()
+	cursor.execute("SELECT comment_id, user_id, picture_id, test, comment_date FROM Comments WHERE picture_id = '{0}'".format(photo_id))
+	return cursor.fetchall() 
+
+def getUserIdFromPhotoId(photo_id):
+	cursor = conn.cursor()
+	cursor.execute("SELECT user_id FROM Pictures WHERE picture_id = '{0}'".format(photo_id))
+	return cursor.fetchone()[0]
+
+@app.route('/makecomments/<album_id>/<photo_id>', methods = ['GET'])
+def comment(album_id, photo_id):
+	return render_template("makecomment.html", message="Create a comment here", albumid=album_id, photoid=photo_id)
+
+@app.route('/makecomments/<album_id>/<photo_id>', methods = ['POST'])
+def make_new_comment(album_id, photo_id):
+	txt = request.form.get("test")
+	date = request.form.get("comment_date")
+	uid = getUserIdFromEmail(flask_login.current_user.id)
+	cursor = conn.cursor()
+	cursor.execute("INSERT INTO Comments(user_id, picture_id, test, comment_date) VALUES ('{0}', '{1}', '{2}', '{3}')".format(uid, photo_id, txt, date))
+	conn.commit()
+	comments = getComments(photo_id) 
+	myComments = []
+	return render_template("comments.html", message="Comment created", albumid=album_id, photoid=photo_id, comments=myComments)
+
+@app.route('/viewcomments/<album_id>/<photo_id>', methods = ['GET'])
+def see_com(album_id, photo_id):
+	comments = getComments(photo_id)
+	myComments = []
+	uid = getUserIdFromEmail(flask_login.current_user.id)
+	photo_uid = getUserIdFromPhotoId(photo_id)
+	return render_template("comments.html", message="Here are the comments for this photo", albumid=album_id, photoid=photo_id, comments=myComments)
+	
+# LIKES #
+
+def getPhotosLikes(photo_id):
+	cursor = conn.cursor()
+	cursor.execute("SELECT user_id, picture_id FROM Likes WHERE picture_id = '{0}'".format(photo_id))
+	return cursor.fetchall() 
+
+@app.route('/viewlikes/<album_id>/<photo_id>', methods = ['GET'])
+@flask_login.login_required
+def see_likes(album_id, photo_id):
+	cursor = conn.cursor()
+	cursor.execute("SELECT COUNT(user_id) FROM Likes WHERE picture_id = '{0}'".format(photo_id))
+	count = cursor.fetchone()[0]
+	myLikes = getPhotosLikes(photo_id) 
+	user = []
+	for i in myLikes:
+		user.append(getUserNameFromId(i[0])) 
+	return render_template("likes.html", message="Here are the likes for this photo", albumid=album_id, photoid=photo_id, user_name=user, numberoflikes=count)
+
+@app.route('/makelikes/<album_id>/<photo_id>', methods = ['POST'])
+@flask_login.login_required
+def make_new_like(album_id, photo_id):
+	uid = getUserIdFromEmail(flask_login.current_user.id)
+	if hasAlreadyLiked(photo_id, uid) == False:
+		cursor = conn.cursor()
+		cursor.execute("INSERT INTO Likes(user_id, picture_id) VALUES ('{0}', '{1}')".format(uid, photo_id))
+		conn.commit()
+		myLikes = getPhotosLikes(photo_id)
+		numberoflikes = len(myLikes) 
+		user_name = []
+		for i in myLikes:
+			user_name.append(getUserNameFromId(i[0]))
+		return render_template("likes.html", message="like created", albumid=album_id, photoid=photo_id, user_name=user_name, num_likes=numberoflikes)
+
+	else:
+		return render_template("likes.html", message="Has already like this photo", albumid=album_id, photoid=photo_id)
+
+def hasAlreadyLiked(photo_id, user_id):
+	cursor = conn.cursor()
+	if cursor.execute("SELECT picture_id, user_id FROM Likes WHERE picture_id = '{0}' AND user_id = '{1}'".format(photo_id, user_id)):
+		return True
+	else:
+		return False
+
+# YOU MAY ALSO LIKE #
+@app.route('/youmayalsolike')
+@flask_login.login_required
+def you_may_also_like():
+	uid =  getUserIdFromEmail(flask_login.current_user.id)
+	cursor = conn.cursor()
+	cursor.execute("SELECT DISTINCT tag_id, COUNT(picture_id) From Tagged_With NATURAL JOIN Pictures WHERE user_id = '{0}' GROUP BY tag_id ORDER BY COUNT(picture_id) DESC LIMIT 5".format(uid))
+	tags = cursor.fetchall() 
+	return render_template("youmayalsolike.html", message="You may also like", like = tags)
+
+# RECOMMENDATIONS #
+@app.route('/friend_recommendation')
+@flask_login.login_required
+def friend_recommendation():
+	uid =  getUserIdFromEmail(flask_login.current_user.id)
+	cursor = conn.cursor()
+	cursor.execute("SELECT email from Users".format(uid))
+	recommended = cursor.fetchall()
+	return render_template("friend_recommendation.html", recommendation = recommended)
 
 #default page
 @app.route("/", methods=['GET'])
 def hello():
 	return render_template('hello.html', message='Welecome to Photoshare')
-
 
 if __name__ == "__main__":
 	#this is invoked when in the shell  you run
--- schema.sql
+++ schema.sql
@@ -1,94 +1,81 @@
 CREATE DATABASE IF NOT EXISTS photoshare;
 USE photoshare;
-DROP TABLE IF EXISTS Users CASCADE;
-DROP TABLE IF EXISTS Albums CASCADE;
-DROP TABLE IF EXISTS Pictures CASCADE;
-DROP TABLE IF EXISTS Tags CASCADE;
-DROP TABLE IF EXISTS Tagged_with CASCADE;
-DROP TABLE IF EXISTS Comments CASCADE;
-DROP TABLE IF EXISTS Likes CASCADE;
-DROP TABLE IF EXISTS Friends CASCADE;
 
 CREATE TABLE Users (
-    user_id int4  AUTO_INCREMENT,
+    user_id int4 AUTO_INCREMENT,
     email varchar(255) UNIQUE NOT NULL,
     password varchar(255) NOT NULL,
     first_name	varchar(255) NOT NULL,
     last_name	varchar(255) NOT NULL,
     dob	date NOT NULL,
     hometown varchar(255) NOT NULL,
-    gender	ENUM('F', 'M') NOT NULL,
-  CONSTRAINT users_pk PRIMARY KEY (user_id)
+    gender varchar(255) NOT NULL,
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE Albums
 (
-album_id int4 AUTO_INCREMENT,
-album_name varchar(255) NOT NULL,
-user_id int4,
-create_date	date NOT NULL,
-FOREIGN KEY (user_id) REFERENCES Users(user_id),
-PRIMARY KEY (album_id)
+	album_id int4 AUTO_INCREMENT,
+	album_name varchar(255) NOT NULL,
+	user_id int4,
+	album_date	date NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	PRIMARY KEY (album_id)
 );
 
 CREATE TABLE Pictures
 (
-  picture_id int4  AUTO_INCREMENT,
-  user_id int4,
-  imgdata longblob ,
-  caption VARCHAR(255),
-  INDEX upid_idx (user_id),
-  CONSTRAINT pictures_pk PRIMARY KEY (picture_id)
+	picture_id int4 AUTO_INCREMENT,
+	user_id int4,
+	imgdata longblob ,
+	caption VARCHAR(255),
+    album_id int,
+    FOREIGN KEY(user_id) references Users(user_id),
+    FOREIGN KEY(album_id) references Albums(album_id),
+	PRIMARY KEY (picture_id)
 );
 
 CREATE TABLE Tags
 (
-word varchar(255),
-PRIMARY KEY (word)
+	tag_id int4 AUTO_INCREMENT,
+	tag varchar(255),
+	PRIMARY KEY (tag_id)
 );
 
-CREATE TABLE Tagged_with
+CREATE TABLE Tagged_With
 (
-picture_id int4,
-word varchar(255),
-FOREIGN KEY (picture_id) REFERENCES Pictures (picture_id),
-FOREIGN KEY (word) REFERENCES Tags (word),
-PRIMARY KEY (picture_id, word)
+	picture_id int4,
+	tag_id int4,
+	FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id),
+	FOREIGN KEY (tag_id) REFERENCES Tags(tag_id)
 );
 
 CREATE TABLE Comments
 (
-comment_id int4 AUTO_INCREMENT,
-test	varchar(255),
-c_owner int4,
-c_date	date,
-picture_id	int4,
-FOREIGN KEY(c_owner) REFERENCES Users(user_id),
-FOREIGN KEY(picture_id) REFERENCES Pictures(picture_id),
-PRIMARY KEY(comment_id)
+	comment_id int4 AUTO_INCREMENT,
+	test varchar(255),
+	comment_owner int4,
+	comment_date date,
+	picture_id	int4,
+	FOREIGN KEY(comment_owner) REFERENCES Users(user_id),
+	FOREIGN KEY(picture_id) REFERENCES Pictures(picture_id),
+	PRIMARY KEY(comment_id)
 );
 
 CREATE TABLE Likes
 (
-user_id int4,
-picture_id int4,
-PRIMARY KEY (photo_id, user_id),
-FOREIGN KEY (user_id) REFERENCES Users(user_id),
-FOREIGN KEY(picture_id) REFERENCES Pictures(picture_id)
+	user_id int4,
+	picture_id int4,
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	FOREIGN KEY(picture_id) REFERENCES Pictures(picture_id),
+    PRIMARY KEY (picture_id, user_id)
 );
 
 CREATE TABLE Friends
 (
-user_id int4,
-friend_id int4,
-FOREIGN KEY (user_id) REFERENCES Users(user_id),
-FOREIGN KEY (friend_id) REFERENCES Users(user_id),
-PRIMARY KEY (user_id, friend_id)
+	user_id int4,
+	friend_id int4,
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	FOREIGN KEY (friend_id) REFERENCES Users(user_id),
+	PRIMARY KEY (user_id, friend_id)
 );
-
-
-
-
-
-INSERT INTO Users (email, password) VALUES ('test@bu.edu', 'test');
-INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test');
